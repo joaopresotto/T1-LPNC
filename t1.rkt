2a3f0ac7e904d86773b938ce6b06351b75ef4f3c
joaopresotto <joaopresotto@gmail.com>
2017-10-22 17:19:39 -0200

    commit inicial

---
+++ t1.rkt
@@ -0,0 +1,6 @@
+;**********************************
+; Membros do grupo:
+;   Gustavo Pires de Moraes
+;   João Gabriel Camacho Presotto
+;   Thiago Costa Leme Rodrigues
+;**********************************
