75d576278951c058e65de0ee0fd78a2969264c56
João Gabriel Camacho Presotto <joaopresotto@gmail.com>
2017-10-25 17:42:07 -0200

    Implementadas novas funções
    Funções para mostrar todas as informações do catálogo de filmes

--- t1.rkt
+++ t1.rkt
@@ -1,3 +1,4 @@
+#lang racket
 ;*********************************************;
 ;  Membros do grupo:                          ;
 ;      Gustavo Pires de Moraes                ;
@@ -12,51 +13,71 @@
 ; como diretor, duração, tema, etc.           ;
 ;*********************************************;
 
-(define jabiru-flix '( '("Blade Runner" "Ficção Científica" "Ridley Scott" '("Harrison Ford" "Rutger Hauer" "Sean Young" "Edward James Olmos" "Daryl Hannah") 1982 "117 min")
-                       '("teste1")
-                       '("teste2")
+(define jabiru-flix '( ("Blade Runner" "Ficção Científica" "Ridley Scott" ("Harrison Ford" "Rutger Hauer" "Sean Young" "Edward James Olmos" "Daryl Hannah") 1982 "117 min")
+                       ("Blade Runner2" "Ficção Científica2" "Ridley Scott2" ("Harrison Ford2" "Rutger Hauer" "Sean Young" "Edward James Olmos" "Daryl Hannah") 1983 "117 min2")
+                       ("Blade Runner3" "Ficção Científica3" "Ridley Scott3" ("Harrison Ford3" "Rutger Hauer" "Sean Young" "Edward James Olmos" "Daryl Hannah") 1984 "117 min3")
                        ))
 
 (define (nome_filme filme)
 	(if (null? filme)
-		(printf "Filme nao cadastrado")
-		(printf "Nome: ~a\n" (caadr filme))))
-
-(define (nomes_filmes)
-  (begin (printf "******************\n") (printf " Nomes dos filmes\n") (printf "******************\n")
-    (map (lambda (filme) (nome_filme filme)) jabiru-flix)))
+		'() ;(printf "Filme nao cadastrado")
+		(car filme) ));(printf "Nome: ~a\n" (caadr filme))))
 
 (define (genero_filme filme)
       (if (null? filme)
-        	(printf "Filme nao cadastrado")
-        	(printf "Genero: ~a\n" (cadr (cadar filme)) )
+        	'() ;(printf "Filme nao cadastrado")
+        	(cadr filme) ;(printf "Genero: ~a\n" (cadr (cadar filme)) )
       )
 )
 
 (define (diretor_filme filme)
       (if (null? filme)
-        	(printf "Filme nao cadastrado")
-        	(printf "Diretor: ~a\n" (caddr (cadar filme)) )
+        	'() ;(printf "Filme nao cadastrado")
+        	(caddr filme) ;(printf "Diretor: ~a\n" (caddr (cadar filme)) )
       )
 )
 
 (define (atores_filme filme)
       (if (null? filme)
-        	(printf "Filme nao cadastrado")
-        	(printf "Atores: ~a\n" (cadddr (cadar filme)) )
+        	'() ;(printf "Filme nao cadastrado")
+        	(cadddr filme) ;(printf "Atores: ~a\n" (cadddr (cadar filme)) )
       )
 )
 
 (define (ano_filme filme)
       (if (null? filme)
-        	(printf "Filme nao cadastrado")
-        	(printf "Ano: ~a\n" (car (cddddr (cadar filme))) )
+        	'() ;(printf "Filme nao cadastrado")
+        	(cadddr (cdr filme)) ;(printf "Ano: ~a\n" (car (cddddr (cadar filme))) )
       )
 )
 
 (define (duracao_filme filme)
       (if (null? filme)
-        	(printf "Filme nao cadastrado")
-        	(printf "Duração: ~a\n" (cadr (cddddr (cadar filme))) )
+        	'() ;(printf "Filme nao cadastrado")
+        	(cadddr (cddr filme));(printf "Duração: ~a\n" (cadr (cddddr (cadar filme))) )
       )
 )
+
+(define (nomes_filmes)
+  (begin (printf "Nomes dos filmes: ")
+    (map (lambda (filme) (nome_filme filme)) jabiru-flix)))
+
+(define (generos_filmes)
+  (begin (printf "Generos dos filmes: ")
+    (map (lambda (filme) (genero_filme filme)) jabiru-flix)))
+
+(define (diretores_filmes)
+  (begin (printf "Diretores dos filmes: ")
+    (map (lambda (filme) (diretor_filme filme)) jabiru-flix)))
+
+(define (atores_filmes)
+  (begin (printf "Atores dos filmes: \n")
+    (map (lambda (filme) (atores_filme filme)) jabiru-flix)))
+
+(define (ano_filmes)
+  (begin (printf "Ano dos filmes: \n")
+    (map (lambda (filme) (ano_filme filme)) jabiru-flix)))
+
+(define (duracao_filmes)
+  (begin (printf "Duracao dos filmes: \n")
+    (map (lambda (filme) (duracao_filme filme)) jabiru-flix)))
