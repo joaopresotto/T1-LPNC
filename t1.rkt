5b26040ee16878d84c12a52913e33b1d775d8edb
joaopresotto <joaopresotto@gmail.com>
2017-10-26 00:13:14 -0200

    COmentários

--- t1.rkt
+++ t1.rkt
@@ -1,4 +1,21 @@
-#lang racket
+;*********************************************;
+ ;  Membros do grupo:                          ;
+ ;      Gustavo Pires de Moraes                ;
+ ;      João Gabriel Camacho Presotto          ;
+ ;      Thiago Costa Leme Rodrigues            ;
+ ;*********************************************;
+
+ ;*********************************************;
+ ; O acervo de filmes é organizado na forma de ;
+ ; uma lista de listas, com cada elemento da   ;
+ ; lista contendo informações sobre o título   ;
+ ; como diretor, duração, tema, etc.           ;
+ ;*********************************************;
+
+; Para rodar no terminal
+; tira o #lang racket do inicio
+; racket -f <nome-do-programa>.rkt -i
+; #lang racket
 
 (define jabiru-flix '( ("Blade Runner" "Sci-Fi" "Ridley Scott" ("Harrison Ford" "Rutger Hauer" "Sean Young") 1982 117)
 
